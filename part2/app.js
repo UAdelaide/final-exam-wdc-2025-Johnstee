f925ecb3468cafb2ee45aef17d040b9946ac385c
Johnstee <will@iptw.net>
2025-06-22 07:07:36 +0000

    [automatic commit] 2025-06-22 07:07:36 UTC 45.249.118.11

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,7 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/')
+app.use('/', userRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
