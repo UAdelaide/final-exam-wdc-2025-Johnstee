453ebad39cc1909f0b620159684ac4d3a017990f
Johnstee <will@iptw.net>
2025-06-22 02:32:01 +0000

    [automatic commit] 2025-06-22 02:32:01 UTC 45.249.118.11

--- part1/app.js
+++ part1/app.js
@@ -101,18 +101,18 @@ let db;
 
     await db.execute(`
 insert into Users(username,email,password_hash,role) values('alice123','alice@example.com','hashed123','owner'),
-insert into Users(username,email,password_hash,role) values('bobwalker','bob@example.com','hashed456','walker');
-insert into Users(username,email,password_hash,role) values('carol123','carol@example.com','hashed789','owner');
-insert into Users(username,email,password_hash,role) values('frederick','fred@example.com','hashed999','owner');
-insert into Users(username,email,password_hash,role) values('emily','emily@example.com','hashed888','walker');
+insert into Users(username,email,password_hash,role) values('bobwalker','bob@example.com','hashed456','walker'),
+insert into Users(username,email,password_hash,role) values('carol123','carol@example.com','hashed789','owner'),
+insert into Users(username,email,password_hash,role) values('frederick','fred@example.com','hashed999','owner'),
+insert into Users(username,email,password_hash,role) values('emily','emily@example.com','hashed888','walker'),
 
-insert into Dogs(name,size,owner_id) values('Max', 'medium', (select user_id from Users where username = 'alice123'));
-insert into Dogs(name,size,owner_id) values('Bella', 'small', (select user_id from Users where username = 'carol123'));
-insert into Dogs(name,size,owner_id) values('Marty', 'large', (select user_id from Users where username = 'alice123'));
-insert into Dogs(name,size,owner_id) values('Clifford', 'large', (select user_id from Users where username = 'frederick'));
-insert into Dogs(name,size,owner_id) values('Pinky', 'small', (select user_id from Users where username = 'emily'));
+insert into Dogs(name,size,owner_id) values('Max', 'medium', (select user_id from Users where username = 'alice123')),
+insert into Dogs(name,size,owner_id) values('Bella', 'small', (select user_id from Users where username = 'carol123')),
+insert into Dogs(name,size,owner_id) values('Marty', 'large', (select user_id from Users where username = 'alice123')),
+insert into Dogs(name,size,owner_id) values('Clifford', 'large', (select user_id from Users where username = 'frederick')),
+insert into Dogs(name,size,owner_id) values('Pinky', 'small', (select user_id from Users where username = 'emily')),
 
-insert into WalkRequests(dog_id,requested_time,duration_minutes,location,status) values((select dog_id from Dogs where name = 'Max'),'2025-06-10 08:00:00',30,'Parklands','open');
+insert into WalkRequests(dog_id,requested_time,duration_minutes,location,status) values((select dog_id from Dogs where name = 'Max'),'2025-06-10 08:00:00',30,'Parklands','open'),
 insert into WalkRequests(dog_id,requested_time,duration_minutes,location,status) values((select dog_id from Dogs where name = 'Bella'),'2025-06-10 09:30:00',45,'Beachside Ave','accepted');
 insert into WalkRequests(dog_id,requested_time,duration_minutes,location,status) values((select dog_id from Dogs where name = 'Marty'),'2025-06-12 05:30:00',42,'Dogpark','accepted');
 insert into WalkRequests(dog_id,requested_time,duration_minutes,location,status) values((select dog_id from Dogs where name = 'Clifford'),'2025-06-14 03:30:00',10,'Dogpark','accepted');
