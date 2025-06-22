b9a321945ddbc04d90cac74f319e4d42e3dcb301
Johnstee <will@iptw.net>
2025-06-22 03:33:09 +0000

    [automatic commit] 2025-06-22 03:33:09 UTC 45.249.118.11

--- part1/app.js
+++ part1/app.js
@@ -145,7 +145,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 
 app.get('/api/walkers/summary', async (req, res) => {
     try {
-        const [walkers]
+        const [walkers] = await db.query(`SELECT * FROM Users WHERE `)
         const [response] = await db.query(`SELECT COUNT(rating_id FROM WalkRatings`);
         res.json(response);
     } catch (err) {
