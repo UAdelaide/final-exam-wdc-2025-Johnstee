c970d80363e29a8cb68e1dbe5ed409944056ddc9
Johnstee <will@iptw.net>
2025-06-22 02:43:38 +0000

    [automatic commit] 2025-06-22 02:43:38 UTC 45.249.118.11

--- part1/app.js
+++ part1/app.js
@@ -23,8 +23,8 @@ let db;
         });
 
         // Create the database if it doesn't exist
-        await connection.query('DROP DATABASE IF EXISTS DogWalkService');
-        await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
+        await connection.query('DROP DATABASE IF EXISTS dogwalkservice');
+        await connection.query('CREATE DATABASE IF NOT EXISTS dogwalkservice');
         await connection.end();
 
         // Now connect to the created database
@@ -32,7 +32,7 @@ let db;
             host: 'localhost',
             user: 'root',
             password: '',
-            database: 'DogWalkService'
+            database: 'dogwalkservice'
         });
 
 
