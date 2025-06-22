b2f011d38a7da334d074fdd6e25aaef80259b5bd
Johnstee <will@iptw.net>
2025-06-22 02:16:13 +0000

    [automatic commit] 2025-06-22 02:16:13 UTC 45.249.118.11

--- part1/app.js
+++ part1/app.js
@@ -43,18 +43,17 @@ let db;
     password_hash VARCHAR(255) NOT NULL,
     role ENUM('owner', 'walker') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    `);
+    `
 
-        // Insert data if table is empty
-        const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
-        if (rows[0].count === 0) {
-            await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
-      `);
-        }
+
+
+
+);
+
+
+
+
+
     } catch (err) {
         console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
     }
