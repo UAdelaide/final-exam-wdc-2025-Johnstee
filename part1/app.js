246ec1fd067aea943e4ce2c643f45ba796d8bb06
Johnstee <will@iptw.net>
2025-06-22 03:39:10 +0000

    [automatic commit] 2025-06-22 03:39:10 UTC 45.249.118.11

--- part1/app.js
+++ part1/app.js
@@ -149,7 +149,7 @@ app.get('/api/walkers/summary', async (req, res) => {
         walkers.forEach((id) => {
             var response = await db.query(`SELECT COUNT(rating_id FROM WalkRatings`);
         });
-        //const [response] = await db.query(`SELECT COUNT(rating_id FROM WalkRatings`);
+        
         res.json(walkers);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch open walk requests' });
