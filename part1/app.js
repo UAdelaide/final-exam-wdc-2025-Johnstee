add98d437c46cdc0eb657f1382a13236bde6d807
Johnstee <will@iptw.net>
2025-06-22 02:51:30 +0000

    [automatic commit] 2025-06-22 02:51:30 UTC 45.249.118.11

--- part1/app.js
+++ part1/app.js
@@ -135,7 +135,7 @@ app.get('/', async (req, res) => {
 
 app.get('/api/dogs/', async (req, res) => {
     try {
-        const [response] = await db.query(`SELECT name, size, users.username FROM Dogs INNER JOIN users ON `);
+        const [response] = await db.query(`SELECT name, size, users.username FROM dogs INNER JOIN users ON dogs.`);
         res.json(response);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch dogs' });
