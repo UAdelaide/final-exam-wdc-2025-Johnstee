0cb02bf67338dfc3066e57368a610267a607bdb4
Johnstee <will@iptw.net>
2025-06-22 06:56:13 +0000

    [automatic commit] 2025-06-22 06:56:13 UTC 45.249.118.11

--- part2/app.js
+++ part2/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
 require('dotenv').config();
 
@@ -8,12 +7,6 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'secret',
-    cookie: {secure: false}
-})
-
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
