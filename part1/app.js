f7a8709d5426f67be8f80c02eed65a2ace3b7627
Johnstee <will@iptw.net>
2025-06-22 02:18:58 +0000

    [automatic commit] 2025-06-22 02:18:58 UTC 45.249.118.11

--- part1/app.js
+++ part1/app.js
@@ -43,8 +43,7 @@ let db;
     password_hash VARCHAR(255) NOT NULL,
     role ENUM('owner', 'walker') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    `
-        );
+    `);
 
 
         await db.execute(`
@@ -54,21 +53,19 @@ let db;
     name VARCHAR(50) NOT NULL,
     size ENUM('small', 'medium', 'large') NOT NULL,
     FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-    `
-        );
+    `);
 
         await db.execute(`
     CREATE TABLE IF NOT EXISTS WalkRequests(
-            request_id INT AUTO_INCREMENT PRIMARY KEY,
-            dog_id INT NOT NULL,
-            requested_time DATETIME NOT NULL,
-            duration_minutes INT NOT NULL,
-            location VARCHAR(255) NOT NULL,
+    request_id INT AUTO_INCREMENT PRIMARY KEY,
+    dog_id INT NOT NULL,
+    requested_time DATETIME NOT NULL,
+    duration_minutes INT NOT NULL,
+    location VARCHAR(255) NOT NULL,
             status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             FOREIGN KEY(dog_id) REFERENCES Dogs(dog_id)
-            `
-        );
+            `);
 
         await db.execute(`
     CREATE TABLE IF NOT EXISTS WalkApplications(
